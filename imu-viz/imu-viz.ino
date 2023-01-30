68356738185ddee6b49fa123bead4557cd693cb1
Josh Roppo <joshroppo@gmail.com>
2023-01-29 20:52:33 -0800

    minor commenting change

--- imu-viz/imu-viz.ino
+++ imu-viz/imu-viz.ino
@@ -29,12 +29,14 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 }
 
 void loop(void) {
+  
   /*
   u8g2.clearBuffer();         // clear the internal memory
   u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
   u8g2.drawStr(4,29,"katie");  // write something to the internal memory
   u8g2.sendBuffer();         // transfer internal memory to the display
   delay(800);
+  */
   
 
   /* Get a new sensor event */ 
